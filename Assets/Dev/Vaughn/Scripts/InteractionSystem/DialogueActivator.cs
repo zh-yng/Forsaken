9161522070b2a331672d9e23493e6471c8e13b70
Kuah Zhao Yang <zykuah2006@gmail.com>
2026-04-16 23:04:56 -0400

    Added item interaction indicator w. extra sprite!

--- Assets/Dev/Vaughn/Scripts/InteractionSystem/DialogueActivator.cs
+++ Assets/Dev/Vaughn/Scripts/InteractionSystem/DialogueActivator.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class DialogueActivator : MonoBehaviour, IInteractable
 {
+    public Material outlineMaterial;
+    public Color outlineColor = Color.white;
+    public float outlineThickness = .2f;
+    private SpriteRenderer outlineRenderer;
     [SerializeField] private DialogueObject dialogueObject;
 
     private bool isDialogueActive;
+
+    private void Start()
+    {
+        // create outline renderer not on object but as a child
+        GameObject outlineObject = new GameObject("Outline");
+        outlineObject.transform.SetParent(transform);
+        outlineObject.transform.localPosition = Vector3.zero;
+        outlineObject.transform.localScale = Vector3.one * (1 + outlineThickness);
+        outlineRenderer = outlineObject.AddComponent<SpriteRenderer>();
+        outlineRenderer.sprite = GetComponent<SpriteRenderer>().sprite;
+        outlineRenderer.material = outlineMaterial;
+        outlineRenderer.color = outlineColor;
+        outlineRenderer.sortingLayerID = GetComponent<SpriteRenderer>().sortingLayerID;
+        outlineRenderer.sortingOrder = GetComponent<SpriteRenderer>().sortingOrder - 1; //
+        outlineRenderer.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && other.TryGetComponent(out PlayerStateMachine player))
         {
             player.Interactable = this;
+            outlineRenderer.enabled = true;
         }
     }
 
@@ -21,6 +42,7 @@ public class DialogueActivator : MonoBehaviour, IInteractable
             if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this)
             {
                 player.Interactable = null;
+                outlineRenderer.enabled = false;
             }
         }
     }
@@ -33,5 +55,8 @@ public class DialogueActivator : MonoBehaviour, IInteractable
     {
         player.DialogueUI.ShowDialogue(dialogueObject);
         isDialogueActive = true;
+
+        // Optionally, you can disable the outline while the dialogue is active
+        outlineRenderer.enabled = false;
     }
 }
